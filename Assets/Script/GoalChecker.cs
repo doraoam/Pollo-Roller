9ff2768adc49d901958f4cf9071b8b190ba829ff
doraoam <doraoam2005@gmail.com>
2015-09-07 16:06:57 +0300

    Add 4 Level
    Add 4 level (Don't touch + Maze)

---
+++ Assets/Script/GoalChecker.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalChecker : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col){
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("You win!!");
+        }
+    }
+}
