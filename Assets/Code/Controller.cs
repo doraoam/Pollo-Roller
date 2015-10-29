e7c305641c6b2c6e616efde49f1527bf0f911611
leafamulet <leaf@leafs-MacBook-Pro.local>
2015-10-29 14:22:04 +0200

    Join File
    Combine work from 2 place together

---
+++ Assets/Code/Controller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+	int speed = 44;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+		if (Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow))
+			transform.Rotate(Vector3.left * speed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
+			transform.Rotate(Vector3.right * speed * Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
+			transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
+			transform.Rotate(Vector3.back * speed * Time.deltaTime);
+		
+	}
+
+}
---
+++ Assets/Code/GameController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject player;
+	public Rigidbody rigit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (player.transform.position.y < -20) {
+			player.transform.position=new Vector3(0,4,0);
+			rigit.velocity = new Vector3(0,0,0);
+
+		}
+	}
+}
